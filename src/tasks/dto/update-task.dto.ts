ec832c18ddb5744c5b97ddaaf93b593cf9855303
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-08-12 23:08:44 +0530

    Fix data validation

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,15 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { Transform } from 'class-transformer';
-import { IsDate, IsEnum } from 'class-validator';
+import { IsEnum } from 'class-validator';
 import { TaskStatus } from 'src/tasks/enums/task-status.enum';
-import { dateTransformer } from 'src/utils/date';
 import { CreateTaskDto } from './create-task.dto';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
   @IsEnum(TaskStatus)
   status?: TaskStatus;
 
-  @Transform(({ value }) => dateTransformer(value))
-  @IsDate()
-  due_date?: Date;
+  completed_at?: Date;
 }
